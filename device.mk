a7d0a0d2f26ae64ce6c09f092b1484ef82c15c9c
Hamster Tian <haotia@gmail.com>
2017-09-19 20:50:40 +0800

    op5: enable IFAAManager again

--- device.mk
+++ device.mk
@@ -238,11 +238,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/vendor/etc/xtwifi.conf
 
 # IFAA (Fingerprint support for Alipay)
-#PRODUCT_PACKAGES += \
-#    org.ifaa.android.manager
+PRODUCT_PACKAGES += \
+    org.ifaa.android.manager
 
-#PRODUCT_BOOT_JARS += \
-#    org.ifaa.android.manager
+PRODUCT_BOOT_JARS += \
+    org.ifaa.android.manager
 
 # IPv6
 PRODUCT_PACKAGES += \
